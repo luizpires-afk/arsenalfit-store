6597e692b5920a4b7cce1acebc8b629d0d05d1c3
luizpires-afk <luiz.pires@aluno.ufop.edu.br>
2026-02-11 23:13:36 -0300

    fix: remove stray character in cart page

--- src/Pages/Cart.tsx
+++ src/Pages/Cart.tsx
@@ -1,4 +1,4 @@
-?import { CSSProperties, useMemo, useState } from 'react';
+import { CSSProperties, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
   ShoppingBag,
